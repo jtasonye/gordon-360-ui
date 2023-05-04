35d7df2c21be2df260d71d43bec2913cf395bb64
Evan Platzer <evan.platzer@gmail.com>
2023-05-04 10:04:56 -0400

    Add React Query client provider

--- src/components/Providers/index.tsx
+++ src/components/Providers/index.tsx
@@ -4,17 +4,24 @@ import 'app.global.css';
 import NetworkContextProvider from 'contexts/NetworkContext';
 import theme from 'theme';
 import AuthProvider from './components/AuthProvider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 const Providers = ({ children }: { children: JSX.Element }) => (
-  <AuthProvider>
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={theme}>
-        <NetworkContextProvider>
-          <UserContextProvider>{children}</UserContextProvider>
-        </NetworkContextProvider>
-      </ThemeProvider>
-    </StyledEngineProvider>
-  </AuthProvider>
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <NetworkContextProvider>
+            <UserContextProvider>{children}</UserContextProvider>
+          </NetworkContextProvider>
+        </ThemeProvider>
+      </StyledEngineProvider>
+    </AuthProvider>
+    <ReactQueryDevtools />
+  </QueryClientProvider>
 );
 
 export default Providers;
